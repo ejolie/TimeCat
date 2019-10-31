b731930c0de23dc51373d4a90152901975a37589
Eunjeong Park <ejolie.dev@gmail.com>
2019-11-01 00:27:53 +0900

    Remove older code and fix conflict in Timeline

--- TimeCat.Client/TimeCat.Web/src/components/Timeline.jsx
+++ TimeCat.Client/TimeCat.Web/src/components/Timeline.jsx
@@ -7,7 +7,7 @@ import moment from 'moment';
 
 const groups = [
   { id: 1, title: 'User', height: 40 },
-  { id: 2, title: 'Project', height: 40 }
+  { id: 2, title: 'Project', height: 40 },
 ];
 
 const items = [
@@ -19,7 +19,7 @@ const items = [
     end_time: moment().add(1, 'hour'),
     backgroundColor: '#E8C1A0',
     icon:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Safari_browser_logo.svg/1028px-Safari_browser_logo.png'
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Safari_browser_logo.svg/1028px-Safari_browser_logo.png',
   },
   {
     id: 2,
@@ -29,7 +29,7 @@ const items = [
     end_time: moment().add(5, 'hour'),
     backgroundColor: '#98E3D5',
     icon:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/512px-Google_Chrome_icon_%28September_2014%29.png'
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/512px-Google_Chrome_icon_%28September_2014%29.png',
   },
   {
     id: 3,
@@ -39,8 +39,8 @@ const items = [
     end_time: moment().add(-1, 'hour'),
     backgroundColor: '#F1E15B',
     icon:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Visual_Studio_Code_1.18_icon.svg/512px-Visual_Studio_Code_1.18_icon.png'
-  }
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Visual_Studio_Code_1.18_icon.svg/512px-Visual_Studio_Code_1.18_icon.png',
+  },
 ];
 
 const itemRenderer = ({
@@ -48,7 +48,7 @@ const itemRenderer = ({
   timelineContext,
   itemContext,
   getItemProps,
-  getResizeProps
+  getResizeProps,
 }) => {
   itemContext.dimensions.height = 30;
   itemContext.selected = false;
@@ -62,8 +62,8 @@ const itemRenderer = ({
           borderRadius: '5px',
           display: 'flex',
           alignItems: 'center',
-          justifyContent: 'center'
-        }
+          justifyContent: 'center',
+        },
       })}
     >
       <img src={item.icon} style={{ width: '18px', height: '18px' }}></img>
@@ -108,70 +108,6 @@ const Timeline = () => {
         />
       </div>
     </>
-=======
-import * as React from 'react';
-import Chart from 'chart.js';
-
-import {
-  scaleDefaults,
-  scale,
-  data,
-  defaults,
-  controller
-} from '../assets/util/TimelineConfig';
-
-Chart.scaleService.registerScaleType('timeline', scale, scaleDefaults);
-Chart.controllers.timeline = Chart.controllers.bar.extend(controller);
-Chart.defaults.timeline = defaults;
-
-const Timeline = () => {
-  let myTimeline;
-  const chartRef = React.useRef();
-
-  const buildChart = () => {
-    const myChartRef = chartRef.current.getContext('2d');
-
-    const config = {
-      type: 'timeline',
-      options: {
-        showText: true,
-        responsive: true,
-        // zoom and pan
-        // pan: {
-        //   enabled: true,
-        //   mode: "x"
-        // },
-        // zoom: {
-        //   enabled: true,
-        //   drag: false,
-        //   mode: "x"
-        //
-        // events: ["mousemove"],
-        maintainAspectRatio: false
-      },
-      data: data
-    };
-
-    // config.data.datasets.forEach((dataset, idx) => {
-    //   // dataset.borderColor = randomColor(0.4);
-    //   // dataset.backgroundColor = randomColor(0.5);
-    //   // dataset.pointBorderColor = randomColor(0.7);
-    //   // dataset.pointBackgroundColor = randomColor(0.5);
-    //   // dataset.pointBorderWidth = 1;
-    // });
-
-    myTimeline = new Chart(myChartRef, config);
-  };
-
-  React.useEffect(() => {
-    // Chart.plugins.register(zoom);
-    buildChart();
-  }, []);
-
-  return (
-    <div style={{ height: '150px' }}>
-      <canvas id='myChart' ref={chartRef} />
-    </div>
   );
 };
 
